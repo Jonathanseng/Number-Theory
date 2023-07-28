73fa890a5b7ab5f3c1f0b13f5a2ca47a03709b14
Seng Keat <corodostudio@gmail.com>
2023-07-28 18:26:10 +0700

    Quardratic example

---
+++ 89. Quadratic Reciprocity Example.py
@@ -0,0 +1,11 @@
+def quadratic_reciprocity_example():
+  """
+  Prints out an example of the Quadratic Reciprocity.
+  """
+
+
+  print("The Legendre symbol of 2 modulo 3 is 1:")
+  print(quadratic_reciprocity(2, 3))
+
+if __name__ == "__main__":
+  quadratic_reciprocity_example()
