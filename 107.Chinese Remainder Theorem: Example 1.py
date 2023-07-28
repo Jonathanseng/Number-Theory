f9915d7870881e2ead859e6f37a99e43741022e4
Seng Keat <corodostudio@gmail.com>
2023-07-28 22:06:45 +0700

    Chinese

---
+++ 107.Chinese Remainder Theorem: Example 1.py
@@ -0,0 +1,14 @@
+def chinese_remainder_theorem_example1(m, a):
+  """Returns an integer x such that x % m[i] = a[i] for all i."""
+  M = m[0]
+  x = a[0]
+  for mi, ai in zip(m[1:], a[1:]):
+    M *= mi
+    x = (x * (M // mi) + ai) % M
+  return x
+
+if __name__ == "__main__":
+  m = [2, 3]
+  a = [1, 2]
+  x = chinese_remainder_theorem_example1(m, a)
+  print(x)
